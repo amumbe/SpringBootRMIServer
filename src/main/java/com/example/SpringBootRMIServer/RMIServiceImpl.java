3af3b6b03aa6ed523ac0f60683cf9276b14eb2c6
Brian-Munene <bmunene@fluidtechglobal.com>
2019-10-29 23:39:08 +0300

    Service message changes

--- src/main/java/com/example/SpringBootRMIServer/RMIServiceImpl.java
+++ src/main/java/com/example/SpringBootRMIServer/RMIServiceImpl.java
@@ -8,7 +8,7 @@ public class RMIServiceImpl implements RMIService {
     @Override
     public String SayHello(String message) {
         System.out.println("==============SERVER IS RUNNING=============");
-        System.out.println("Inside RMI Impl - Incoming message : " + message);
-        return "Here is the message: "+message+"  ::Response time=>"+new Date();
+        System.out.println("Inside RMI Impl - Incoming message from mars : " + message);
+        return "Message from the Martians: "+message+"  ::Response time=>"+new Date();
     }
 }
